b327d5ab6e0c50a8b0d1984aecaba3e76c0afd21
mostafa-de <mostafafayyado1@gmail.com>
2023-03-19 23:07:55 +0300

    * Add loading component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,30 @@
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import type {AppProps} from "next/app";
+import {useEffect, useState} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "@/styles/global.css";
+import Loading from "@/components/Loading";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    AOS.init({
-      duration: 2000,
-    });
-  }, []);
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        AOS.init({
+            duration: 2000,
+        });
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+            {loading ? <Loading/> : <Component {...pageProps} />}
+        </>
+    );
 }
 
 export default MyApp;
