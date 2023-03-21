f84e93eec4c9ececc8867a47a679f5fac67543a6
mostafa-de <mostafafayyado1@gmail.com>
2023-03-21 23:54:26 +0300

    * Fix bug in carousel on first image loaded. * Fix loading style on mobile.

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,19 +1,30 @@
 import Carousel from 'react-material-ui-carousel'
 import Box from "@mui/material/Box";
 import Image from "next/image";
+import {useState} from "react";
 
 export default function CarouselProject(): JSX.Element {
-    const items = [{img: "/sons.jpeg"}, {img: "/mostafa1.png"}, {img: "/test.jpg"}]
+    const [firstImgLoaded, setFirstImgLoaded] = useState(false);
+    const items = [{img: "/sons.jpeg"}, {img: "/cover-2.jpg"}]
 
     return (
-        <Box>
-            <Carousel>
-                {items.map((item, idx) => (
-                    <Box key={idx}>
-                        <Image src={item.img} width={560} height={300} alt='test'/>
-                    </Box>
-                ))}
-            </Carousel>
-        </Box>
+        <>
+            <img src={items[0].img}
+                 width={560}
+                 height={300}
+                 alt='test'
+                 onLoad={() => setFirstImgLoaded(true)}
+                 style={{display: "none"}}
+            />
+            {firstImgLoaded && (
+                <Carousel autoPlay swipe={false}>
+                    {items.map((item, idx) => (
+                        <Box key={idx}>
+                            <Image src={item.img} width={560} height={300} alt='test'/>
+                        </Box>
+                    ))}
+                </Carousel>
+            )}
+        </>
     );
 };
